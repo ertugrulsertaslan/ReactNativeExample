5cf98d20d10b4c570baa13201776379654b7a863
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-10-31 20:29:55 +0300

    change login page and create signup page

--- components/screens/LoginPage.tsx
+++ components/screens/LoginPage.tsx
@@ -3,15 +3,32 @@ import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import Loading from "@/components/Loading";
 
-const LoginPage = () => {
-  const [name, setName] = useState("");
-  const [lastName, setLastName] = useState("");
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+type RootStackParamList = {
+  Login: undefined;
+  SignUp: undefined;
+  Home: undefined;
+};
+
+type LoginScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "Login"
+>;
+
+interface Props {
+  navigation: LoginScreenNavigationProp;
+}
+
+const LoginPage: React.FC<Props> = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [result, setResult] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = () => {
     setIsLoading(true);
-    setResult(`${name} ${lastName}`);
+    setResult(`${email} ${password}`);
 
     setTimeout(() => {
       setIsLoading(false);
@@ -20,19 +37,24 @@ const LoginPage = () => {
   return (
     <View style={styles.container}>
       <Text>Welcome {result}</Text>
-      <Text>Name</Text>
+      <Text>Email</Text>
       <TextInput
+        keyboardType="email-address"
         style={styles.textInputStyle}
-        placeholder="Enter Your Name"
+        placeholder="Enter Your Email"
         placeholderTextColor="black"
-        onChangeText={(value) => setName(value)}
+        onChangeText={(value) => setEmail(value)}
+        value={email}
       />
-      <Text>Last Name</Text>
+      <Text>Password</Text>
       <TextInput
+        keyboardType="default"
         style={styles.textInputStyle}
-        placeholder="Enter Your LastName"
+        secureTextEntry={true}
+        placeholder="Enter Your Password"
         placeholderTextColor="black"
-        onChangeText={(value) => setLastName(value)}
+        onChangeText={(value) => setPassword(value)}
+        value={password}
       />
       <Pressable
         onPress={handleSubmit}
@@ -43,7 +65,19 @@ const LoginPage = () => {
           styles.buttonStyle,
         ]}
       >
-        <Text style={styles.buttonText}>Send</Text>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </Pressable>
+
+      <Pressable
+        onPress={() => navigation.navigate("SignUp")}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? "gray" : "lightgray",
+          },
+          styles.signUpButton,
+        ]}
+      >
+        <Text style={styles.signUpButtonText}>Sign Up</Text>
       </Pressable>
       {isLoading ? <Loading /> : null}
     </View>
@@ -75,8 +109,21 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  buttonText: {
+  loginButtonText: {
     fontWeight: "bold",
     color: "white",
+  },
+  signUpButtonText: {
+    fontWeight: "bold",
+    color: "black",
+  },
+  signUpButton: {
+    borderWidth: 1,
+    width: "80%",
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
   },
 });
