05f4c5400b37e0adf06f9088b5db4128c36d5cc4
Ertuğrul Sertaslan <ertugrulsertaslan@gmail.com>
2024-11-02 15:10:53 +0300

    add Firebase data insertion functionality to project

--- components/screens/HomePage.tsx
+++ components/screens/HomePage.tsx
@@ -1,10 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
+import { CustomButton } from "../utils";
 const HomePage = () => {
+  const sendData = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "reactNativeLesson"), {
+        title: "Zero to Hero",
+        content: "React Native tutorial for beginner",
+        lesson: 95,
+      });
+      console.log("Document written with ID:", docRef.id);
+    } catch (e) {
+      console.error("Error adding document", e);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>HomePage</Text>
+      <CustomButton
+        title="Save"
+        buttonColor="blue"
+        pressedButtonColor="gray"
+        handleOnPress={sendData}
+      />
     </View>
   );
 };
